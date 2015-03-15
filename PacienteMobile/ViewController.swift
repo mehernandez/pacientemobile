40aa8b9263b8f9dc921a734941a77ccb3b527162
Mario Hernandez <med.hernandez10@gmail.com>
2015-03-14 22:15:07 -0500

    Initial Commit

---
+++ PacienteMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PacienteMobile
+//
+//  Created by Mario Hernandez on 14/03/15.
+//  Copyright (c) 2015 Equinox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
