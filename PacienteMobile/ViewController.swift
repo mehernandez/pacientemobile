6b058fac859b2bf04d488deb610c68f35bb27681
Mario Hernandez <med.hernandez10@gmail.com>
2015-03-15 09:00:18 -0500

    Primeras vistas y primer código para el HTTP Request

--- PacienteMobile/ViewController.swift
+++ PacienteMobile/ViewController.swift
@@ -19,6 +19,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func logear() {
+        
+        var request = HTTPTask()
+        request.GET("http://vluxe.io", parameters: nil, success: {(response: HTTPResponse) in
+            if response.responseObject != nil {
+                let data = response.responseObject as NSData
+                let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("response: \(str)") //prints the HTML of the page
+            }
+            },failure: {(error: NSError) in
+                println("error: \(error)")
+        })
+        
+    }
+    
 
 
 }
