47cb0f497a140c5b3bbe5b4775df34d262646c84
Mario Hernandez <med.hernandez10@gmail.com>
2015-03-16 08:16:32 -0500

    Ahora graba sonido temporal y lo reproduce a los 5 s

--- PacienteMobile/NuevoEpisodioViewController.swift
+++ PacienteMobile/NuevoEpisodioViewController.swift
@@ -19,12 +19,194 @@ AVAudioPlayerDelegate, AVAudioRecorderDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
 }
+    
+    // Github
+    
+    func audioPlayerBeginInterruption(player: AVAudioPlayer!) {
+        /* The audio session is deactivated here */
+    }
+    
+    func audioPlayerEndInterruption(player: AVAudioPlayer!,
+        withOptions flags: Int) {
+            if flags == AVAudioSessionInterruptionFlags_ShouldResume{
+                player.play()
+            }
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!,
+        successfully flag: Bool){
+            
+            if flag{
+                println("Audio player stopped correctly")
+            } else {
+                println("Audio player did not stop correctly")
+            }
+            
+            audioPlayer = nil
+            
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!,
+        successfully flag: Bool){
+            
+            if flag{
+                
+                println("Successfully stopped the audio recording process")
+                
+                /* Let's try to retrieve the data for the recorded file */
+                var playbackError:NSError?
+                var readingError:NSError?
+                
+                let fileData = NSData(contentsOfURL: audioRecordingPath(),
+                    options: .MappedRead,
+                    error: &readingError)
+                
+                /* Form an audio player and make it play the recorded data */
+                audioPlayer = AVAudioPlayer(data: fileData, error: &playbackError)
+                
+                /* Could we instantiate the audio player? */
+                if let player = audioPlayer{
+                    player.delegate = self
+                    
+                    /* Prepare to play and start playing */
+                    if player.prepareToPlay() && player.play(){
+                        println("Started playing the recorded audio")
+                    } else {
+                        println("Could not play the audio")
+                    }
+                    
+                } else {
+                    println("Failed to create an audio player")
+                }
+                
+            } else {
+                println("Stopping the audio recording failed")
+            }
+            
+            /* Here we don't need the audio recorder anymore */
+            self.audioRecorder = nil;
+            
+    }
+    
+    
+    func audioRecordingPath() -> NSURL{
+        
+        let fileManager = NSFileManager()
+        
+        let documentsFolderUrl = fileManager.URLForDirectory(.DocumentDirectory,
+            inDomain: .UserDomainMask,
+            appropriateForURL: nil,
+            create: false,
+            error: nil)
+        
+        return documentsFolderUrl!.URLByAppendingPathComponent("Recording.m4a")
+        
+    }
+    
+    func audioRecordingSettings() -> [NSObject : AnyObject]{
+        
+        /* Let's prepare the audio recorder options in the dictionary.
+        Later we will use this dictionary to instantiate an audio
+        recorder of type AVAudioRecorder */
+        
+        return [
+            AVFormatIDKey : kAudioFormatMPEG4AAC as NSNumber,
+            AVSampleRateKey : 16000.0 as NSNumber,
+            AVNumberOfChannelsKey : 1 as NSNumber,
+            AVEncoderAudioQualityKey : AVAudioQuality.Low.rawValue as NSNumber
+        ]
+        
+    }
+    
+    func startRecordingAudio(){
+        
+        var error: NSError?
+        
+        let audioRecordingURL = self.audioRecordingPath()
+        
+        audioRecorder = AVAudioRecorder(URL: audioRecordingURL,
+            settings: audioRecordingSettings(),
+            error: &error)
+        
+        if let recorder = audioRecorder{
+            
+            recorder.delegate = self
+            /* Prepare the recorder and then start the recording */
+            
+            if recorder.prepareToRecord() && recorder.record(){
+                
+                println("Successfully started to record.")
+                
+                /* After 5 seconds, let's stop the recording process */
+                let delayInSeconds = 5.0
+                let delayInNanoSeconds =
+                dispatch_time(DISPATCH_TIME_NOW,
+                    Int64(delayInSeconds * Double(NSEC_PER_SEC)))
+                
+                dispatch_after(delayInNanoSeconds, dispatch_get_main_queue(), {
+                    [weak self] in
+                    self!.audioRecorder!.stop()
+                })
+                
+            } else {
+                println("Failed to record.")
+                audioRecorder = nil
+            }
+            
+        } else {
+            println("Failed to create an instance of the audio recorder")
+        }
+        
+    }
+    
+
+    
+    
+    @IBAction func grabar(sender: UIButton) {
+        
+        
+        /* Ask for permission to see if we can record audio */
+        
+        var error: NSError?
+        let session = AVAudioSession.sharedInstance()
+        
+        if session.setCategory(AVAudioSessionCategoryPlayAndRecord,
+            withOptions: .DuckOthers,
+            error: &error){
+                
+                if session.setActive(true, error: nil){
+                    println("Successfully activated the audio session")
+                    
+                    session.requestRecordPermission{[weak self](allowed: Bool) in
+                        
+                        if allowed{
+                            self!.startRecordingAudio()
+                        } else {
+                            println("We don't have permission to record audio");
+                        }
+                        
+                    }
+                } else {
+                    println("Could not activate the audio session")
+                }
+                
+        } else {
+            
+            if let theError = error{
+                println("An error occurred in setting the audio " +
+                    "session category. Error = \(theError)")
+            }
+            
+        }
+        
+    }
 
 }
